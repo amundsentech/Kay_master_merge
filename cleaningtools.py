8183b50afe88d4edaf32ca6e2a9df4d15bb6d031
sebastian coombs <63413957+sebastiancoombs@users.noreply.github.com>
2022-06-28 22:49:31 -0600

    Cleaned Data push
    This includes all the cleaning tools and the main.py file for creating the cleaned data .csv

--- cleaningtools.py
+++ cleaningtools.py
@@ -1,13 +1,80 @@
 import pandas as pd
+import kaytools.filter_config as config
 
-def pull_sample_ids(data,id_formats=['[a-zA-Z]{1}[0-9]{1}','[a-zA-Z]{2}[0-9]{1}','[a-zA-Z]{3}[0-9]{1}']):
+def pull_sample_ids(data,id_formats=config.sample_id_formats):
+    data=data.replace(' ','')
+    data.index=data.index.astype(int)
     sample_ids=[samp for samp in list(data.columns) if ('samp' in samp.lower())]
-    data_clean=pd.DataFrame()
-    data_clean['sample_id']=0
-    for col in sample_ids:
-        for i in range(len(id_formats)):
-                data[col]=data[col].astype(str)
-                data['is_id']=data[col].str.contains(id_formats[i])
-                data_clean['sample_id']=pd.concat([data_clean['sample_id'],data[col][data['is_id']==True]].copy(),ignore_index=True)
+    print(f'get sample ids from {sample_ids},with forms{id_formats}')
+    data['sample_id']=''
+    sample_cols=[]
+    for i in range(len(id_formats)):
+        i_format=id_formats[i]
+        for col in sample_ids:
+            print(f'search {col} for format {i_format}')
+            
+            data[col]=data[col].astype(str)
+            data[col]=data[col].fillna('')
+            if data[col].str.contains(i_format).any():
+                sample_cols.append(col)
+                print(f'{col} has format{i_format}')
+                extract=data[col].str.extract(i_format).fillna('')[0]
+                data['sample_id']=data['sample_id']+extract
+            else: 
+                print(f'{col} is not an id column')
+    print(sample_cols)
+    data=data.drop(sample_cols,axis=1)
+    for col in data.columns:
+        if data[col].isna().sum()==len(data):
+            print(f'Drop {col}: no data')
+            data=data.drop(col,axis=1)
+    ##data=data.fillna('')        
+    data=data.sort_values('sample_id')
+    data=data.set_index('sample_id')
+    return data
 
-    return data_clean.sort_values()
+def column_cleanup(data,mapping=config.depth_mapping):
+    data=data.astype(str)
+    for value in mapping.values():
+        if value in data.columns:
+            data[value].fillna('')
+        else: 
+            data[value]=''
+    for key ,value in mapping.items():
+        print (f'add {key} to {value} and drop {key}')
+        try:
+            data[value]=data[value]+data[key]
+            data=data.drop(key,axis=1)
+        except Exception as e:
+            print(e)
+    return data
+
+def carrot_cleanup(data):
+    carrots=['>','<']
+    for col in data.columns:
+        print(col)
+        data[col]=data[col].astype(str).str.strip()
+        more_index=data[col][data[col].str.startswith(carrots[0])].index
+        less_index=data[col][data[col].str.startswith(carrots[-1])].index
+        try:
+
+            data.loc[less_index,col]=0.0
+            data2=data.loc[more_index][col].str.replace(carrots[0],'')
+            data.loc[more_index][col]=data2
+            number=data2.unique()[0]
+        except Exception as e: 
+            
+            print(e)
+        col2=f'{col}_2'
+        
+        if col2 in data.columns:
+            try:
+                new_index=data[data[col2]!=''].index
+                data.loc[new_index][col]=data[new_index][col2]*data.loc[new_index][col]
+            except Exception as e: 
+                print('No Carrots')
+                print(e)
+    return data
+
+            
+
--- main.py
+++ main.py
@@ -1,3 +1,50 @@
-import cleaningtools as ct
+import getopt,sys
+
+import argparse
+import pandas as pd
+import kaytools.cleaningtools as ct
+import kaytools.filter_config as config
+import datetime
 
 
+msg = "Begin Cleaning"
+# create a parser object
+parser = argparse.ArgumentParser(description = msg)
+parser.add_argument("spectral", nargs = '*', metavar = "spectral_path", type = str,
+                     help = "add the path to the spectral_master data csv")
+parser.add_argument("assays", nargs = '*', metavar = "spectral_path", type = str,
+                     help = "add the path to the assay_master data csv")
+args = parser.parse_args()
+
+id_formats= config.sample_id_formats
+
+assays=pd.read_csv('../_Master Databases/drill assays master.csv')
+spectral=pd.read_csv('../_Master Databases/spectral master.csv',low_memory=False)
+
+assays=pd.read_csv(args.spectral)
+spectral=pd.read_csv(args.assays,low_memory=False)
+
+date=datetime.date().strftime('%Y_%m_%d')
+
+spectral=ct.pull_sample_ids(spectral)
+
+assays=ct.pull_sample_ids(assays)
+
+
+spectral=ct.column_cleanup(spectral,mapping=config.depth_mapping)
+spectral=ct.column_cleanup(spectral,mapping=config.file_mapping)
+spectral=ct.column_cleanup(spectral,mapping=config.vnir_mapping)
+spectral=spectral.fillna('')
+spectral=spectral.replace('nan','')
+
+assays=ct.carrot_cleanup(assays)
+assays=ct.column_cleanup(assays,mapping=config.depth_mapping)
+assays=ct.column_cleanup(assays,mapping=config.file_mapping)
+assays=assays.fillna('')
+
+assays=assays.replace('nan','')
+
+
+
+final=pd.merge(assays,spectral,on='sample_id',how='inner',sort=True)
+final.to_csv(f'Master_Spectral_Assay_Merge_Clean_{date}')
