48c94ca2a2fa6ff39e88cb334598d9869cc5c69e
sebastian coombs <63413957+sebastiancoombs@users.noreply.github.com>
2022-07-11 10:37:33 -0600

    file dir

--- filter_config.py
+++ filter_config.py
@@ -1,13 +1,13 @@
 
 
-output_path='/amcdrive/_AZ_Kay/_Master Databases/'
-assay_file='/amcdrive/_AZ_Kay/_Master Databases/drill assays master'
-spec_file='/amcdrive/_AZ_Kay/_Master Databases/spectral master.csv'
-alter_file='/amcdrive/_AZ_Kay/_Master Databases/alteration master.csv'
-mineral_file='/amcdrive/_AZ_Kay/_Master Databases/mineralization master.csv'
-sample_file='/amcdrive/_AZ_Kay/_Master Databases/samples master.csv'
-structure_file='/amcdrive/_AZ_Kay/_Master Databases/stricture master.csv'
-lith_file='/amcdrive/_AZ_Kay/_Master Databases/lithology master.csv'
+output_path='~/amcdrive/_AZ_Kay/_Master Databases/'
+assay_file='~/amcdrive/_AZ_Kay/_Master Databases/drill assays master'
+spec_file='~/amcdrive/_AZ_Kay/_Master Databases/spectral master.csv'
+alter_file='~/amcdrive/_AZ_Kay/_Master Databases/alteration master.csv'
+mineral_file='~/amcdrive/_AZ_Kay/_Master Databases/mineralization master.csv'
+sample_file='~/amcdrive/_AZ_Kay/_Master Databases/samples master.csv'
+structure_file='~/amcdrive/_AZ_Kay/_Master Databases/stricture master.csv'
+lith_file='~/amcdrive/_AZ_Kay/_Master Databases/lithology master.csv'
 
 assay_fname=f'drill assays master.csv'
 spec_fname=f'spectral master.csv'
